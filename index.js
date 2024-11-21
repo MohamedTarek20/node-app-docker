a68903b37a16541f1f9d908fec252caed9bc5b97
mohamedtarekismail95 <mohamedtarekismail95@gmail.com>
2024-11-22 01:56:25 +0200

    Mongo DB and  NPM Mongoose

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = 4000;
 const app = express();
 
-app.get('/',(req,res)=>res.send('<h1>Hello World hi Dev Two</h1>'));
+// connect to database
+const DB_USER = "root"
+const DB_PASSWORD = "example"
+const DB_PORT = 27017;
+const DB_HOST = "mongo"
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+mongoose.connect(URI).then(() => console.log('Database connected')).catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('<h1>Hello World hi Dev Two</h1>'));
 app.listen(PORT, '0.0.0.0', () => console.log(`App runnning on port ${PORT}`))
