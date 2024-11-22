af4c4ce14ab706ce0720f27154fc091caf89642b
mohamedtarekismail95 <mohamedtarekismail95@gmail.com>
2024-11-22 02:02:43 +0200

    Redis and NPM Redis

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
 
 const PORT = 4000;
 const app = express();
+
+//connect to redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis"
+
+const redisClient = redis.createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+
+redisClient.on('error', (err) => console.log('Redis not connected : ', err));
+redisClient.on('connect', () => console.log('Redis connected'));
+redisClient.connect();
 
 // connect to database
 const DB_USER = "root"
@@ -13,5 +26,13 @@ const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 
 mongoose.connect(URI).then(() => console.log('Database connected')).catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('<h1>Hello World hi Dev Two</h1>'));
-app.listen(PORT, '0.0.0.0', () => console.log(`App runnning on port ${PORT}`))
+app.get('/', (req, res) => {
+    redisClient.set('products', 'hello world');
+    res.send('<h1>Hello World hi Dev Two</h1>')
+});
+
+app.get('/data', async(req, res) => {
+    const data = await redisClient.get('products');
+    res.send(`<h1>Hello World hi Dev Two</h1><h2>${data}</h2>`)
+});
+app.listen(PORT, () => console.log(`App runnning on port ${PORT}`))
